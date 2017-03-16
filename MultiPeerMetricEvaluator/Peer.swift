64ccd21b90237e3f227aadb7213fbaf060dd285e
psychobolt <psychobolt@gmail.com>
2017-03-15 17:51:01 -0700

    Code

---
+++ MultiPeerMetricEvaluator/Peer.swift
@@ -0,0 +1,9 @@
+//
+//  Peer.swift
+//  MultiPeerMetricEvaluator
+//
+//  Created by Michael Tran on 3/2/17.
+//  Copyright © 2017 Example. All rights reserved.
+//
+
+import Foundation
---
+++ MultiPeerMetricEvaluator/PeerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PeerTableViewCell.swift
+//  MultiPeerMetricEvaluator
+//
+//  Created by Michael Tran on 3/2/17.
+//  Copyright © 2017 Example. All rights reserved.
+//
+
+import UIKit
+
+class PeerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
